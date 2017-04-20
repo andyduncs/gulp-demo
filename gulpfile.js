dbd1c9ff61701ea438f80ec0342d0d15f0cebc03
Andy Duncan <andyduncan2003@gmail.com>
2017-04-20 22:54:33 +1000

    Added coffee script files

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,13 @@
 var gulp = require('gulp'),
+    gutil = require ('gulp-util'),
+    coffee = require('gulp-coffee');
 
-    gutil = require ('gulp-util');
+var coffeeSources = [ 'components/coffee/tagline.coffee' ];
 
-gulp.task('log', function(){
-    gutil.log('Workflow example')
+gulp.task('coffee', function(){
+    gulp.src('components/coffee.tagline.coffee')
+    .pipe(coffee({ bare: true   })
+        .on('error' , gutil.log)
+    .pipe(gulp.dest('components/scripts'))
+    )
 });
